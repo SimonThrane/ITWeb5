f93b45f773dd4a8db384ae172caa786bcd4cf1cd
SimonThrane <minijordan23-virum@hotmail.com>
2017-10-29 08:45:38 +0100

    Added controller to Components and ComponentTypes

--- EmbeddedStock2/Models/Component.cs
+++ EmbeddedStock2/Models/Component.cs
@@ -1,12 +1,17 @@
-﻿namespace EmbeddedStock2.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmbeddedStock2.Models
 {
     public class Component
     {
+        [Display(Name = "Id")]
         public long ComponentId { get; set; }
+        [Display (Name = "Type")]
         public long ComponentTypeId { get; set; }
         public int ComponentNumber { get; set; }
         public string SerialNo { get; set; }
         public ComponentStatus Status { get; set; }
+        [Display(Name = "Comment")]
         public string AdminComment { get; set; }
         public string UserComment { get; set; }
         public long? CurrentLoanInformationId { get; set; }
